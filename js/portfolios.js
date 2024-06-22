9baa493c3507b5004ca4aea327c95dc2ebad736c
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-06-23 02:01:05 +0530

    Portfolio updated v2

--- js/portfolios.js
+++ js/portfolios.js
@@ -1,213 +1,274 @@
 var portfolio_data = [
-  {
-    id: "portfolio-smart-energy-monitoring",
-    images: [
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring.png',
-        subHtml: '<h4>Smart Energy Monitoring App</h4><p>Final Year Project</p>',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/1.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/1.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/2.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/2.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/3.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/3.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/4.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/4.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/5.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/5.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/6.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/6.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/7.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/7.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/8.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/8.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/9.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/9.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/10.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/10.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/11.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/11.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/12.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/12.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/13.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/13.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/14.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/14.png',
-        subHtml: '<h4>Image 1 title</h4><p>Image 1 descriptions.</p>',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/15.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/15.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/16.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/16.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/17.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/17.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/18.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/18.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/19.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/19.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/20.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/20.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/21.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/21.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/22.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/22.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/23.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/23.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/24.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/24.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/25.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/25.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-smart-energy-monitoring-images/26.png',
-        thumb: '../portfolio/images/portfolio-smart-energy-monitoring-images/26.png',
-      },
-    ]
-  },
-  {
-    id: "portfolio-newpages-merchant",
-    images: [
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant.png',
-        subHtml: '<h4>Merchant Management System</h4><p>Newpages Network</p>',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/1.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/1.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/2.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/2.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/3.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/3.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/4.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/4.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/5.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/5.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/6.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/6.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/7.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/7.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/8.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/8.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/9.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/9.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/10.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/10.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/11.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/11.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-newpages-merchant-images/12.png',
-        thumb: '../portfolio/images/portfolio-newpages-merchant-images/12.png',
-      },
-    ]
-  },
-  {
-    id: "portfolio-payroll",
-    images: [
-      {
-        src: '../portfolio/images/portfolio-payroll.png',
-        thumb: '../portfolio/images/portfolio-payroll.png',
-        subHtml: '<h4>Payroll System</h4><p>Coursework</p>',
-      },
-      {
-        src: '../portfolio/images/portfolio-payroll-images/1.png',
-        thumb: '../portfolio/images/portfolio-payroll-images/1.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-payroll-images/2.png',
-        thumb: '../portfolio/images/portfolio-payroll-images/2.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-payroll-images/3.png',
-        thumb: '../portfolio/images/portfolio-payroll-images/3.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-payroll-images/4.png',
-        thumb: '../portfolio/images/portfolio-payroll-images/4.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-payroll-images/5.png',
-        thumb: '../portfolio/images/portfolio-payroll-images/5.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-payroll-images/6.png',
-        thumb: '../portfolio/images/portfolio-payroll-images/6.png',
-      },
-      {
-        src: '../portfolio/images/portfolio-payroll-images/7.png',
-        thumb: '../portfolio/images/portfolio-payroll-images/7.png',
-      },
-    ]
-  },
-]
+	{
+		id: 'projects-pizza-wizza',
+		images: [
+			{
+				src: './images/PizzaWizzaSlider/01.png',
+				thumb: './images/PizzaWizzaSlider/01.png',
+				subHtml: '<h4>Landing Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/02.png',
+				thumb: './images/PizzaWizzaSlider/02.png',
+				subHtml: '<h4>Pizza Section</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/03.png',
+				thumb: './images/PizzaWizzaSlider/03.png',
+				subHtml:
+					'<h4>Side & Beverages Section</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/04.png',
+				thumb: './images/PizzaWizzaSlider/04.png',
+				subHtml: '<h4>Cart Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/05.png',
+				thumb: './images/PizzaWizzaSlider/05.png',
+				subHtml: '<h4>Orders Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/06.png',
+				thumb: './images/PizzaWizzaSlider/06.png',
+				subHtml: '<h4>Signup Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/07.png',
+				thumb: './images/PizzaWizzaSlider/07.png',
+				subHtml: '<h4>Login Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/08.png',
+				thumb: './images/PizzaWizzaSlider/08.png',
+				subHtml:
+					'<h4>Admin Create Data Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+		],
+	},
+	{
+		id: 'expense-management-tracker',
+		images: [
+			{
+				src: './images/ExpenseTrackerSlider/01.png',
+				thumb: './images/ExpenseTrackerSlider/01.png',
+				subHtml:
+					'<h4>Dashboard Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/02.png',
+				thumb: './images/ExpenseTrackerSlider/02.png',
+				subHtml:
+					'<h4>Search Feature</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/03.png',
+				thumb: './images/ExpenseTrackerSlider/03.png',
+				subHtml:
+					'<h4>Transaction Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/04.png',
+				thumb: './images/ExpenseTrackerSlider/04.png',
+				subHtml:
+					'<h4>Profile Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/05.png',
+				thumb: './images/ExpenseTrackerSlider/05.png',
+				subHtml:
+					'<h4>Wallet Page with Dark Mode</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/06.png',
+				thumb: './images/ExpenseTrackerSlider/06.png',
+				subHtml:
+					'<h4>Dashboard Page with Dark Mode</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/07.png',
+				thumb: './images/ExpenseTrackerSlider/07.png',
+				subHtml:
+					'<h4>Add Expense Component</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/08.png',
+				thumb: './images/ExpenseTrackerSlider/08.png',
+				subHtml:
+					'<h4>Add Income Component</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/09.png',
+				thumb: './images/ExpenseTrackerSlider/09.png',
+				subHtml:
+					'<h4>SignIn User Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/10.png',
+				thumb: './images/ExpenseTrackerSlider/10.png',
+				subHtml:
+					'<h4>SignUp User Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/ExpenseTrackerSlider/11.png',
+				thumb: './images/ExpenseTrackerSlider/11.png',
+				subHtml:
+					'<h4>Forget User Page</h4><p>Expense Tracker Management Project</p>',
+			},
+		],
+	},
+	{
+		id: 'prajs-ngo-project',
+		images: [
+			{
+				src: './images/PrajsNgoSlider/01.png',
+				thumb: './images/PrajsNgoSlider/02.png',
+				subHtml: '<h4>Landing Page</h4><p>Prajs NGO Project</p>',
+			},
+			{
+				src: './images/PrajsNgoSlider/02.png',
+				thumb: './images/PrajsNgoSlider/02.png',
+				subHtml: '<h4>Recent Causes Section</h4><p>Prajs NGO Project</p>',
+			},
+			{
+				src: './images/PrajsNgoSlider/03.png',
+				thumb: './images/PrajsNgoSlider/03.png',
+			},
+			{
+				src: './images/PrajsNgoSlider/04.png',
+				thumb: './images/PrajsNgoSlider/04.png',
+			},
+			{
+				src: './images/PrajsNgoSlider/05.png',
+				thumb: './images/PrajsNgoSlider/05.png',
+			},
+			{
+				src: './images/PrajsNgoSlider/06.png',
+				thumb: './images/PrajsNgoSlider/06.png',
+			},
+			{
+				src: './images/PrajsNgoSlider/07.png',
+				thumb: './images/PrajsNgoSlider/07.png',
+				subHtml: '<h4>Footer Section</h4><p>Prajs NGO Project</p>',
+			},
+		],
+	},
+	{
+		id: 'splice-awwwards-project',
+		images: [
+			{
+				src: './images/SpliceSlider/01.png',
+				thumb: './images/SpliceSlider/01.png',
+				subHtml:
+					'<h4>Landing Page</h4><p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/02.png',
+				thumb: './images/SpliceSlider/02.png',
+				subHtml:
+					'<h4>Loader Page</h4><p>Splice Awwwards Music Band Web Design</p>',
+			},
+
+			{
+				src: './images/SpliceSlider/03.png',
+				thumb: './images/SpliceSlider/03.png',
+				subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/04.png',
+				thumb: './images/SpliceSlider/04.png',
+				subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/05.png',
+				thumb: './images/SpliceSlider/05.png',
+				subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/06.png',
+				thumb: './images/SpliceSlider/06.png',
+				subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/07.png',
+				thumb: './images/SpliceSlider/07.png',
+				subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/08.png',
+				thumb: './images/SpliceSlider/08.png',
+				subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/09.png',
+				thumb: './images/SpliceSlider/09.png',
+				subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/SpliceSlider/10.png',
+				thumb: './images/SpliceSlider/10.png',
+				subHtml:
+					'<h4>Footer Section</h4><p>Splice Awwwards Music Band Web Design</p>',
+			},
+		],
+	},
+	{
+		id: 'thierrychopain-portfolio-project',
+		images: [
+			{
+				src: './images/ThierrySlider/01.png',
+				thumb: './images/ThierrySlider/01.png',
+				subHtml:
+					'<h4>Landing Page</h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/02.png',
+				thumb: './images/ThierrySlider/02.png',
+				subHtml:
+					'<h4>Branding Video Section</h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/03.png',
+				thumb: './images/ThierrySlider/03.png',
+				subHtml:
+					'<h4></h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/04.png',
+				thumb: './images/ThierrySlider/04.png',
+				subHtml:
+					'<h4></h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/05.png',
+				thumb: './images/ThierrySlider/05.png',
+				subHtml:
+					'<h4></h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/06.png',
+				thumb: './images/ThierrySlider/06.png',
+				subHtml:
+					'<h4></h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/07.png',
+				thumb: './images/ThierrySlider/07.png',
+				subHtml:
+					'<h4></h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/08.png',
+				thumb: './images/ThierrySlider/08.png',
+				subHtml:
+					'<h4></h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+			{
+				src: './images/ThierrySlider/09.png',
+				thumb: './images/ThierrySlider/09.png',
+				subHtml:
+					'<h4></h4><p>ThierryChopain Developer Awwwards Portfolio</p>',
+			},
+		],
+	},
+];
